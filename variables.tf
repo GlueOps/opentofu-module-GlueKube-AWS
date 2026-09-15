d6d005ff060947e036ff3d6ff624bb3b45da49bc
Bazmahou <bouissihamza6@gmail.com>
2026-09-15 14:50:50 +0000

    chore: bumped the default gluekube tag version

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "gluekube_docker_image" {
 variable "gluekube_docker_tag" {
   type        = string
   description = "Docker tag for GlueKube"
-  default     = "v1.34.5-gluekube.32"
+  default     = "v1.34.11-gluekube.2"
 }
 
 variable "vpc_cidr_block" {
